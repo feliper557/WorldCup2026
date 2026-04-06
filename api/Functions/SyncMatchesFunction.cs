b78551b1041cc18b7e2eb30b9556206f7f866188
Felipe rodriguez <feliper421@gmail.com>
2026-04-06 07:35:44 -0500

    Fix: add missing using directives for OpenAPI attributes

--- api/Functions/SyncMatchesFunction.cs
+++ api/Functions/SyncMatchesFunction.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using WorldCup.Api.Extensions;
 using WorldCup.Api.Infrastructure.Repositories.Interfaces;
